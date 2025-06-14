c37690b26a226cf6fe93e787344449c4d1dea4f2
Yash Debnath <ydebnath25@gmail.com>
2025-06-14 12:12:51 +0530

    ci/cd node js

--- my-node-api/index.js
+++ my-node-api/index.js
@@ -3,9 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello from server!");
+  res.send("Updated from GitHub CI/CD!");
+});
+
+app.get('/status', (req, res) => {
+  res.json({ status: 'Running', timestamp: new Date() });
 });
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running at http://35.154.219.143:${port}`);
+  console.log(`Server is live at http://35.154.219.143:${port}`);
 });
